d56989898be5b4b1e21c1ace483b8be449694d11
Jose Acosta <116679757+JPAcost@users.noreply.github.com>
2022-11-26 00:16:37 -0600

    Cambios descritos en el readme

--- src/Interfaz/OperacionesPila.java
+++ src/Interfaz/OperacionesPila.java
@@ -1,48 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Interfaz;
 
 
 //Codigo Andres
 public class OperacionesPila {
     
-    private NodoPila Pila = new NodoPila();
+    NodoPila Pilas[] = new NodoPila[5];
     
     OperacionesPila(){
         
-        Pila = null;
-        
+        for(int i = 0; i < Pilas.length;Pilas[i] = null, i++);   
     }
     
-
-    public void insertarInicio(int valor) {
+//Creo que para trabajar mejor en esto podemos usar la matriz debido a que cada pais podria tener su numero de cuidad
+    /*
+   NCuidad =1    Roma
+    NCuidad= 2  Naples
+     NCuidad=3   Milan
+     NCuidad=4   Venicia
+     NCuidad=5   Florencia
+    es decir cada vez que un tren vaya a roma guardamos las personas en Roma en int 1
+    */
+    
+    
+    public void insertarInicio(int NCuidad,int valor) {
         
         NodoPila nuevo = new NodoPila();
-        nuevo.tren = valor;
+        nuevo.cuidad = valor;
         nuevo.siguiente = null;
         
         //Evalua si la pila es Null o no, si es null solo le 
         //anade el valor de nuevo, si no es null se lo anade
         // al siguiente espacion en memoria
-        if (Pila == null)
-            Pila = nuevo;
+        if (Pilas[NCuidad] == null)
+            Pilas[NCuidad] = nuevo;
         else {
-            nuevo.siguiente = Pila;
-            Pila = nuevo;
+            nuevo.siguiente = Pilas[NCuidad];
+            Pilas[NCuidad] = nuevo;
         }
         
     }
-    
-
-    public void borrarInicio () {
+    //Implementacion de vector de pilas tomada de la ultima clase que tuvimos de estructura de datos
+    public void borrarInicio (int Ncuidad) {
         
         //Checkea si la 
-        if (Pila != null)
-                Pila = Pila.siguiente;
+        if (Pilas[Ncuidad] != null)
+                Pilas[Ncuidad] = Pilas[Ncuidad].siguiente;
         
     }
+   
     
     
 }
