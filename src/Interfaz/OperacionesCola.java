0e6bc54ea762562138ecfdf042e95b2a93370291
Jose Acosta <116679757+JPAcost@users.noreply.github.com>
2022-11-30 19:14:05 -0600

    Cambios para manejar una cola por cada cuidad

--- src/Interfaz/OperacionesCola.java
+++ src/Interfaz/OperacionesCola.java
@@ -1,26 +1,21 @@
-
-
-package Interfaz;
-
-
 public class OperacionesCola {
     
-    private NodoCola Cola = new NodoCola();
+  
+     NodoCola Colas[] = new NodoCola[6];
     
     OperacionesCola(){
         
-        Cola = null;
-        
+         for(int i = 0; i < Colas.length;Colas[i] = null, i++); 
     }
 
-    public void insertarFinal (int Persona) {
+    public void insertarFinal (int Cuidad,int destino) {
         NodoCola nuevo = new NodoCola();
-        nuevo.persona = Persona;
+        nuevo.persona = destino;
         nuevo.siguiente = null;
-        if (Cola == null)
-            Cola = nuevo;
+        if (Colas[Cuidad] == null)
+            Colas[Cuidad] = nuevo;
         else {   
-            NodoCola aux = Cola;
+            NodoCola aux = Colas[Cuidad];
             while (aux.siguiente != null)
                 aux = aux.siguiente;
             aux.siguiente = nuevo;
@@ -28,15 +23,32 @@ public class OperacionesCola {
         }  
     }
     
-    public void borrarInicio () {
+    public void borrarInicio (int Inicio) {
 
-        if (Cola != null)
-            Cola = Cola.siguiente;
+        if (Colas[Inicio] != null)
+            Colas[Inicio] = Colas[Inicio].siguiente;
         
+    }
+       public void imprimir() {
+        for(int i = 1; i < Colas.length; i++) {
+            switch (i) {
+                case 1 -> System.out.println("Roma");
+                case 2 -> System.out.println("Naples");
+                case 3 -> System.out.println("Milan ");
+                case 4 -> System.out.println("Venicia");
+                case 5 -> System.out.println("Florencia");
+                default -> {
+                }
+            }
+           
+            for(NodoCola aux = Colas[i]; aux != null; aux = aux.siguiente)
+                System.out.println(aux.persona);
+        }
     }
     
     //contarPersonas devuelve un dato tipo int el cual es el numero de 
     //personas que hay en la fila
+    /*
     public int contarPersonas(){
         
         int cuenta = 0;
@@ -56,5 +68,7 @@ public class OperacionesCola {
             }
         
     }
+    codigo en mantenimiento jajaja
+    */
     
 }
