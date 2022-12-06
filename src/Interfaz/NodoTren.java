68cfddaa6f823dd4eb20cad4132754bc046c74f3
Jose Acosta <116679757+JPAcost@users.noreply.github.com>
2022-12-06 17:46:11 -0600

    solo tiene un nodo mas pero aun no se utiliza

--- src/Interfaz/NodoTren.java
+++ src/Interfaz/NodoTren.java
@@ -6,9 +6,9 @@ public class NodoTren {
     
     //agregamos destino para poder hacer la validacion de de ciudad a la que 
     //tiene que ir
-     public int destino;
-     
-     public int cuidad;
+    public int PersonasTren;
+     public int cuidadDestino;
+     public int CuidadOrigen;
      public NodoTren siguiente;
      
 }
